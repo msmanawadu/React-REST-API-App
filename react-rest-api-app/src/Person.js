0171fd0f9461f067e210600a787a58be6d48f506
Sandeepa Manawadu <msmanawadu@gmail.com>
2020-10-24 10:58:50 +0530

    updated the error catch block for isLoaded:

--- react-rest-api-app/src/Person.js
+++ react-rest-api-app/src/Person.js
@@ -24,7 +24,7 @@ class Person extends Component {
             },
             (error) => {
                 this.setState({
-                    isLoaded: true,
+                    isLoaded: false,
                     error
                 });
             } 
