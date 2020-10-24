c1ba7fb8535e5dccc132a7065338d88319d182aa
Sandeepa Manawadu <msmanawadu@gmail.com>
2020-10-24 11:05:46 +0530

    updated the error code

--- react-rest-api-app/src/Person.js
+++ react-rest-api-app/src/Person.js
@@ -24,7 +24,7 @@ class Person extends Component {
             },
             (error) => {
                 this.setState({
-                    isLoaded: false,
+                    isLoaded: true,
                     error
                 });
             } 
